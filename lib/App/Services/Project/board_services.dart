3e9a7e5d3e9c35be4fc0244843d4c7e0d9ff3616
Mohamed S Abdrabou <masedup@gmail.com>
2023-01-13 01:08:22 -0500

    board services

---
+++ lib/App/Services/Project/board_services.dart
@@ -0,0 +1,44 @@
+import 'package:flutterpp/App/Models/board_model.dart';
+import 'package:flutterpp/App/Providers/Network/Project/board_provider.dart';
+import 'package:flutterpp/App/Services/Global/call_pipeline.dart';
+
+class BoardServices {
+  final BoardProvider _boardProvider = BoardProvider();
+  final CallPipeline _callPipeline = CallPipeline();
+
+  // get board by project id
+  Future<List<BoardModel>?> getBoards({required int projectId}) async {
+    List<BoardModel>? boards = await _callPipeline.futurePipeline(
+      future: () => _boardProvider.getBoards(projectId: projectId),
+      name: 'getBoards',
+    );
+
+    return boards;
+  }
+
+  // create board
+  Future<BoardModel?> createBoard({
+    required String name,
+    required int projectId,
+  }) async {
+    BoardModel? board = await _callPipeline.futurePipeline(
+      future: () => _boardProvider.createBoard(
+        name: name,
+        projectId: projectId,
+      ),
+      name: 'createBoard',
+    );
+
+    return board;
+  }
+
+  // update board
+  Future<BoardModel?> updateBoard({required BoardModel board}) async {
+    BoardModel? updatedBoard = await _callPipeline.futurePipeline(
+      future: () => _boardProvider.updateBoard(board: board),
+      name: 'updateBoard',
+    );
+
+    return updatedBoard;
+  }
+}
--- lib/App/Services/Project/project_services.dart
+++ lib/App/Services/Project/project_services.dart
@@ -1,5 +1,5 @@
 import 'package:flutterpp/App/Models/project_model.dart';
-import 'package:flutterpp/App/Providers/Network/project_provider.dart';
+import 'package:flutterpp/App/Providers/Network/Project/project_provider.dart';
 import 'package:flutterpp/App/Services/Global/call_pipeline.dart';
 
 class ProjectServices {
