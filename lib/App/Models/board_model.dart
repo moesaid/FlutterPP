bfa406dc3162091156265eb861fc4135e8064c01
Mohamed S Abdrabou <masedup@gmail.com>
2023-01-13 01:05:34 -0500

    board provider

---
+++ lib/App/Models/board_model.dart
@@ -0,0 +1,32 @@
+class BoardModel {
+  int? id, index, projectId;
+  String? name, createdAt, updatedAt;
+
+  BoardModel({
+    this.id,
+    this.projectId,
+    this.name,
+    this.index,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  BoardModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    projectId = json['project_id'];
+    index = json['index'];
+    name = json['name'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['name'] = name;
+    data['index'] = index;
+    data['project_id'] = projectId;
+    data['created_at'] = createdAt;
+    data['updated_at'] = updatedAt;
+    return data;
+  }
+}
--- lib/App/Providers/Network/Project/board_provider.dart
+++ lib/App/Providers/Network/Project/board_provider.dart
@@ -1,1 +1,76 @@
-class BoardProvider {}
+import 'dart:convert';
+
+import 'package:flutterpp/App/Models/board_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class BoardProvider {
+  final supabase = Supabase.instance.client;
+
+  // create board
+  Future<BoardModel?> createBoard({
+    required String name,
+    required int projectId,
+  }) async {
+    List<Map> data = await supabase.from('boards').insert([
+      {
+        'name': name,
+        'project_id': projectId,
+        'created_at': DateTime.now().toIso8601String(),
+      }
+    ]).select();
+
+    if (data.isEmpty) return null;
+    var localJson = json.encode(data[0]);
+    return BoardModel.fromJson(json.decode(localJson));
+  }
+
+  // get boards by project id
+  Future<List<BoardModel>?> getBoards({required int projectId}) async {
+    List<Map> data = await supabase
+        .from('boards')
+        .select('*')
+        .eq('project_id', projectId)
+        .select();
+
+    if (data.isEmpty) return [];
+
+    List<BoardModel> boards = [];
+    for (var item in data) {
+      var localJson = json.encode(item);
+      boards.add(BoardModel.fromJson(json.decode(localJson)));
+    }
+
+    return boards;
+  }
+
+  // update board
+  Future<BoardModel?> updateBoard({required BoardModel board}) async {
+    List<Map> data = await supabase
+        .from('boards')
+        .update({
+          'name': board.name,
+          'updated_at': DateTime.now().toIso8601String(),
+        })
+        .eq('id', board.id)
+        .select();
+
+    if (data.isEmpty) return null;
+    var localJson = json.encode(data[0]);
+    return BoardModel.fromJson(json.decode(localJson));
+  }
+
+  // delete board
+  Future<bool> deleteBoard({required int boardId}) async {
+    List<Map> data = await supabase
+        .from('boards')
+        .delete()
+        .eq(
+          'id',
+          boardId,
+        )
+        .select();
+
+    if (data.isEmpty) return false;
+    return true;
+  }
+}
