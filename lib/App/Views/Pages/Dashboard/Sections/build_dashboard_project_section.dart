7a040d751ace2217d3ffeb614023d7cfa2f1ec25
moesaid <masedup@gmail.com>
2024-01-29 15:26:08 -0500

    BuildDashboardProjectSection

---
+++ lib/App/Views/Pages/Dashboard/Sections/build_dashboard_project_section.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutterpp/App/Controllers/dashboard_controller.dart';
+import 'package:flutterpp/App/Models/project_model.dart';
+import 'package:flutterpp/App/Views/Pages/Dashboard/dashboard_page.dart';
+import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
+
+class BuildDashboardProjectSection extends StatelessWidget {
+  const BuildDashboardProjectSection({
+    super.key,
+    required this.controller,
+  });
+
+  final DashboardController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('projects'.capitalize!),
+        SizedBox(height: 3.sp),
+        Row(
+          children: [
+            BuildDashboardCreateProject(controller: controller),
+            SizedBox(width: 5.sp),
+            Expanded(
+              child: SizedBox(
+                height: 180,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: controller.projects.length,
+                  separatorBuilder: (context, index) => SizedBox(
+                    width: 5.sp,
+                  ),
+                  itemBuilder: (context, index) {
+                    ProjectModel item = controller.projects[index];
+                    return BuildDashboardProjectItem(
+                      controller: controller,
+                      item: item,
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/App/Views/Pages/Dashboard/dashboard_page.dart
+++ lib/App/Views/Pages/Dashboard/dashboard_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutterpp/App/Controllers/dashboard_controller.dart';
 import 'package:flutterpp/App/Models/project_model.dart';
 import 'package:flutterpp/App/Views/Global/build_loading_switch.dart';
+import 'package:flutterpp/App/Views/Pages/Dashboard/Widgets/build_dashboard_numbers_item.dart';
 import 'package:flutterpp/App/Views/Pages/Project/Pages/project_create_page.dart';
 import 'package:flutterpp/App/Views/Pages/Project/Widgets/build_project_avatar.dart';
 import 'package:flutterpp/Helpers/colors_helper.dart';
@@ -73,94 +74,6 @@ class DashboardPage extends GetView<DashboardController> {
           ),
         );
       },
-    );
-  }
-}
-
-class BuildDashboardNumbersItem extends StatelessWidget {
-  const BuildDashboardNumbersItem({
-    super.key,
-    required this.controller,
-  });
-
-  final DashboardController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(2.sp),
-        color: Get.theme.colorScheme.primaryContainer,
-        border: Border.all(
-          width: 1,
-          color: Get.theme.colorScheme.secondaryContainer,
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(10.sp),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'team members'.capitalize!,
-                        style: TextStyle(
-                          fontSize: 4.sp,
-                          fontWeight: FontWeight.w400,
-                          color: Get.theme.colorScheme.onBackground.withOpacity(
-                            0.3,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        controller.teamMembers.length.toString(),
-                        style: TextStyle(
-                          fontSize: 10.sp,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(1.sp),
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.indigo,
-                    ),
-                    child: Icon(
-                      Icons.supervised_user_circle_rounded,
-                      size: 10.sp,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const Divider(thickness: 0.5, height: 0),
-          Padding(
-            padding: EdgeInsets.all(1.sp),
-            child: TextButton(
-              onPressed: () => print('object'),
-              child: Text(
-                'view all'.capitalize!,
-                style: TextStyle(
-                  fontSize: 3.6.sp,
-                  color: Get.theme.colorScheme.onBackground,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
