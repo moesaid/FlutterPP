a1226d3675661ed592c8a1100eff6695afd9e4e4
moesaid <masedup@gmail.com>
2024-02-01 14:55:19 -0500

    Create build_single_project_no_path_state.dart

---
+++ lib/App/Views/Pages/Project/Widgets/build_single_project_no_path_state.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:sizer/sizer.dart';
+
+class BuildSingleProjectNoPathState extends StatelessWidget {
+  const BuildSingleProjectNoPathState({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Lottie.asset(
+          'assets/lottie/empty_box.json',
+          height: 200,
+          width: 200,
+        ),
+        SizedBox(height: 2.sp),
+        Text(
+          'No local path found - your code\'s gone on a cosmic vacation! 🚀🤷',
+          style: Get.textTheme.titleMedium!.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 1.sp),
+        Text(
+          'create a new project or load an existing one',
+          style: Get.textTheme.bodyMedium,
+        ),
+      ],
+    );
+  }
+}
