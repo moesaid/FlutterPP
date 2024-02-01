2cb5512c9fbc097315c992a5afd4fc1ef172744c
moesaid <masedup@gmail.com>
2024-01-31 22:03:03 -0500

    Update build_single_project_header.dart

--- lib/App/Views/Pages/Project/Widgets/build_single_project_header.dart
+++ lib/App/Views/Pages/Project/Widgets/build_single_project_header.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterpp/App/Controllers/Project/project_single_controller.dart';
-import 'package:flutterpp/App/Views/Pages/Project/Widgets/build_project_avatar.dart';
+import 'package:flutterpp/App/Views/Pages/Project/Widgets/build_single_project_head_info.dart';
 import 'package:flutterpp/App/Views/Pages/Project/Widgets/build_single_project_parked_at.dart';
 import 'package:flutterpp/Helpers/colors_helper.dart';
 import 'package:get/get.dart';
@@ -45,66 +45,6 @@ class BuildSingleProjectHeader extends StatelessWidget {
           const BuildSingleProjectParkedAt(),
         ],
       ),
-    );
-  }
-}
-
-class BuildSingleProjectHeadInfo extends StatelessWidget {
-  const BuildSingleProjectHeadInfo({
-    super.key,
-    required this.controller,
-  });
-
-  final ProjectSingleController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        BuildProjectAvatar(
-          icon: controller.project.icon ?? '',
-          colors: [
-            ColorHelper.hexToColor(controller.project.color1 ?? '#000000'),
-            ColorHelper.hexToColor(controller.project.color2 ?? '#000000'),
-          ],
-        ),
-        SizedBox(width: 6.sp),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Text(
-                  controller.project.title ?? '',
-                  style: Get.textTheme.titleLarge,
-                ),
-                IconButton.filledTonal(
-                  onPressed: () => print('object'),
-                  style: ButtonStyle(
-                    visualDensity: VisualDensity.compact,
-                    iconColor: MaterialStateProperty.all(
-                      Get.theme.colorScheme.onPrimaryContainer,
-                    ),
-                    iconSize: MaterialStateProperty.all(5.sp),
-                    backgroundColor: MaterialStateProperty.all(
-                      Colors.transparent,
-                    ),
-                  ),
-                  icon: const Icon(
-                    Icons.edit,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 1.sp),
-            Text(
-              controller.project.description ?? '',
-              style: Get.textTheme.bodyMedium,
-            ),
-            SizedBox(height: 1.sp),
-          ],
-        ),
-      ],
     );
   }
 }
