1980d625eff6b768f6445f9022e6c4bac74fb286
moesaid <masedup@gmail.com>
2024-01-31 22:02:54 -0500

    BuildSingleProjectHeadInfo

--- lib/App/Views/Pages/Project/Widgets/build_single_project_head_info.dart
+++ lib/App/Views/Pages/Project/Widgets/build_single_project_head_info.dart
@@ -1,53 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutterpp/App/Controllers/Project/project_single_controller.dart';
 import 'package:flutterpp/App/Views/Pages/Project/Widgets/build_project_avatar.dart';
-import 'package:flutterpp/App/Views/Pages/Project/Widgets/build_single_project_parked_at.dart';
 import 'package:flutterpp/Helpers/colors_helper.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
-
-class BuildSingleProjectHeader extends StatelessWidget {
-  const BuildSingleProjectHeader({
-    super.key,
-    required this.controller,
-  });
-
-  final ProjectSingleController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: context.width,
-      padding: EdgeInsets.all(10.sp),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            ColorHelper.hexToColor(controller.project.color1 ?? '#000000')
-                .withOpacity(0.1),
-            ColorHelper.hexToColor(controller.project.color2 ?? '#000000')
-                .withOpacity(0.06),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-        color: Get.theme.colorScheme.secondaryContainer,
-        border: Border(
-          bottom: BorderSide(
-            color: Get.theme.colorScheme.secondaryContainer,
-            width: 1,
-          ),
-        ),
-      ),
-      child: Row(
-        children: [
-          BuildSingleProjectHeadInfo(controller: controller),
-          const Spacer(),
-          const BuildSingleProjectParkedAt(),
-        ],
-      ),
-    );
-  }
-}
 
 class BuildSingleProjectHeadInfo extends StatelessWidget {
   const BuildSingleProjectHeadInfo({
