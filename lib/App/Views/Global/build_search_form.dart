e269807477c6e76f75902d12ba0696b187ddf5d3
moesaid <masedup@gmail.com>
2024-02-10 22:17:57 -0500

    Create build_search_form.dart

---
+++ lib/App/Views/Global/build_search_form.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
+
+class BuildSearchForm extends StatelessWidget {
+  final void Function(String)? onSearch;
+  final void Function()? onClear;
+  final String? searchKey;
+  const BuildSearchForm({
+    super.key,
+    this.onSearch,
+    this.searchKey,
+    this.onClear,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormBuilderState>();
+    return FormBuilder(
+      key: formKey,
+      child: Container(
+        width: context.width * 0.2,
+        clipBehavior: Clip.antiAlias,
+        padding: EdgeInsets.symmetric(
+          horizontal: 4.sp,
+          vertical: 2.sp,
+        ),
+        decoration: BoxDecoration(
+          color: Get.theme.colorScheme.primaryContainer,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: FormBuilderTextField(
+                name: 'name',
+                initialValue: searchKey,
+                scrollPadding: EdgeInsets.zero,
+                cursorHeight: 15,
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 2.sp,
+                    horizontal: 0,
+                  ),
+                  isDense: true,
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  hintStyle: Get.textTheme.bodyMedium?.copyWith(
+                    color: Get.theme.colorScheme.onBackground.withOpacity(0.4),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 2.sp),
+            InkWell(
+              onTap: () {
+                if (searchKey != null && searchKey!.isNotEmpty) {
+                  onClear?.call();
+                  return;
+                }
+
+                if (formKey.currentState?.saveAndValidate() == true) {
+                  onSearch?.call(formKey.currentState?.value['name']);
+                }
+              },
+              child: Icon(
+                (searchKey != null && searchKey!.isNotEmpty)
+                    ? Icons.close
+                    : Icons.search_rounded,
+                size: 6.sp,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
