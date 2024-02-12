e7ac05ceb07bc90927201711d1f33f8a31d59d14
moesaid <masedup@gmail.com>
2024-02-12 06:21:14 -0500

    Create build_invoice_single_sidebar.dart

---
+++ lib/App/Views/Pages/Invoice/Widgets/build_invoice_single_sidebar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
+
+class BuildInvoiceSingleSidebar extends StatelessWidget {
+  final VoidCallback onPrint, onDownload, onEdit;
+  const BuildInvoiceSingleSidebar({
+    super.key,
+    required this.onPrint,
+    required this.onDownload,
+    required this.onEdit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 1,
+      child: Padding(
+        padding: EdgeInsets.all(6.0.sp),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            FilledButton.icon(
+              onPressed: onPrint,
+              icon: Icon(Icons.print, size: 6.sp),
+              label: Text('print'.capitalize!),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  Get.theme.colorScheme.secondary,
+                ),
+              ),
+            ),
+            SizedBox(height: 3.sp),
+            FilledButton.icon(
+              onPressed: onDownload,
+              icon: Icon(Icons.file_download, size: 6.sp),
+              label: Text('download'.capitalize!),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  Get.theme.colorScheme.secondary.withOpacity(0.6),
+                ),
+              ),
+            ),
+            Divider(thickness: 0.5, height: 14.sp),
+            FilledButton.icon(
+              onPressed: onEdit,
+              icon: Icon(Icons.email, size: 6.sp),
+              label: Text('Edit'.capitalize!),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  Get.theme.colorScheme.primaryContainer,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
