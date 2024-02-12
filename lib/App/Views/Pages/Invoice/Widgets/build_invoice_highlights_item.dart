c300911f667cf9082fb02c9a4e4de0c8264850b9
moesaid <masedup@gmail.com>
2024-02-12 06:19:00 -0500

    Create build_invoice_highlights_item.dart

---
+++ lib/App/Views/Pages/Invoice/Widgets/build_invoice_highlights_item.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
+
+class BuildInvoiceHighlightsItem extends StatelessWidget {
+  final String? title, value;
+  final bool? isAmount;
+  const BuildInvoiceHighlightsItem({
+    super.key,
+    this.title,
+    this.value,
+    this.isAmount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Flex(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.end,
+      direction: Axis.horizontal,
+      children: [
+        Flexible(
+          flex: 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Flexible(
+                child: Text(
+                  isAmount != null
+                      ? title?.toUpperCase() ?? ''
+                      : title?.capitalize ?? '',
+                  style: TextStyle(
+                    fontSize: 5.sp,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(width: 6.sp),
+        Flexible(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Flexible(
+                child: Text(
+                  value ?? '',
+                  style: TextStyle(
+                    fontSize: 5.sp,
+                    color: isAmount != null
+                        ? Colors.black
+                        : Colors.black.withOpacity(0.6),
+                    fontWeight: isAmount != null ? FontWeight.bold : null,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
