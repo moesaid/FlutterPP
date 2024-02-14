b3e145aaf7c216cffdd44ec6d1cca80ab7da13ff
moesaid <masedup@gmail.com>
2024-02-14 01:48:20 -0500

    Executable update #4

--- macos/ci_scripts/ci_post_clone.sh
+++ macos/ci_scripts/ci_post_clone.sh
@@ -16,8 +16,6 @@ flutter precache --macos
 # Install Flutter dependencies.
 flutter pub get
 
-flutter build macos
-
 # Install CocoaPods using Homebrew.
 HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
 brew install cocoapods
