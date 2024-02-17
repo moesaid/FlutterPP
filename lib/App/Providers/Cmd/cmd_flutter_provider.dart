e5505624987b2f9eaeb51b6f66b5e5325fa6a9c7
moesaid <masedup@gmail.com>
2024-02-17 04:31:21 -0500

    add path

--- lib/App/Providers/Cmd/cmd_flutter_provider.dart
+++ lib/App/Providers/Cmd/cmd_flutter_provider.dart
@@ -1,60 +1,13 @@
 import 'dart:io';
 
-import 'package:flutterpp/App/Views/Global/build_snackbar.dart';
 import 'package:flutterpp/Storage/local_flutter_path.dart';
-import 'package:get/get.dart';
 import 'package:process_run/cmd_run.dart';
 
 class CmdFlutterProvider {
   // get flutter local path
   Future<String?> getFlutterLocalPath() async {
     try {
-      String? path = LocalFlutterPath().read();
-      if (path != null) return path;
-
-      // get dart path from env variables using shell
-      String? res;
-
-      if (GetPlatform.isWindows) {
-        String command = 'for %A in ("%PATH:;=" "%") do @echo %~A';
-        ProcessResult result = await Process.run('cmd', ['/c', command]);
-
-        List<String> paths = result.stdout.toString().split(';');
-
-        // find dart path
-        for (var path in paths) {
-          if (path.contains('flutter')) {
-            res = path;
-            break;
-          }
-        }
-      } else if (GetPlatform.isMacOS) {
-        String command = 'echo \$PATH | tr \':\' \'\\n\'';
-        ProcessResult result = await Process.run('/bin/bash', ['-c', command]);
-
-        List<String> paths = result.stdout.toString().split('\n');
-
-        // find flutter path
-        for (var path in paths) {
-          if (path.contains('flutter')) {
-            res = path;
-            break;
-          }
-        }
-      }
-
-      if (res == null || res.isEmpty) {
-        BuildSnackBar(
-          title: 'Error',
-          message: path.toString(),
-        ).error();
-        return null;
-      }
-
-      // write to local file
-      LocalFlutterPath().write(res);
-
-      return res;
+      return LocalFlutterPath().read();
     } catch (e) {
       print('Error getting Flutter local path: $e');
       return null;
