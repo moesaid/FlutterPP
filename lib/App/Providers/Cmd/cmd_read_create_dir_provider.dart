a3e4084a4b67e394c13f9a352bf3adc9b45b67b2
moesaid <masedup@gmail.com>
2024-02-29 22:57:15 -0500

    Update cmd_read_create_dir_provider.dart

--- lib/App/Providers/Cmd/cmd_read_create_dir_provider.dart
+++ lib/App/Providers/Cmd/cmd_read_create_dir_provider.dart
@@ -13,7 +13,7 @@ class CmdReadCreateDirProvider {
     String? option,
   }) async {
     try {
-      String command = GetPlatform.isWindows ? 'tree /F' : 'ls';
+      String command = GetPlatform.isWindows ? 'tree' : 'ls';
 
       // var result = await runExecutableArguments(command, [option ?? '', path]);
 
